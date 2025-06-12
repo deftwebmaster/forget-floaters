b995ae3169940d756a9d2f4c409906ef7296814a
deft <dwgit@fastmail.com>
2025-06-12 18:57:12 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Forget Floaters: Retrain Your Brain";
+export const SITE_DESCRIPTION = "Forget Floaters is a practical guide that teaches you how to retrain your brain to ignore eye floaters naturally—without surgery, gimmicks, or expensive treatments.";
